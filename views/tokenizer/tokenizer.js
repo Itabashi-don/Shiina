b45a66e5ca4c6f0f6b6a5cd5f586b26ccd813452
Genbu Hase <hasegenbu@gmail.com>
2018-07-10 17:56:40 +0900

    views/tokenizer製作

--- views/tokenizer/tokenizer.js
+++ views/tokenizer/tokenizer.js
@@ -2,8 +2,31 @@
 const textInputter = document.getElementById("tokenizer-text");
 /** @type {HTMLButtonElement} */
 const tokenizeBtn = document.getElementById("tokenizer-tokenize");
+/** @type {HTMLButtonElement} */
+const clearBtn = document.getElementById("tokenizer-clear");
 /** @type {HTMLTableElement} */
 const resultTable = document.getElementById("tokenizer-result");
+/** @type {HTMLTableSectionElement} */
+const resultList = resultTable.querySelector("TBody");
+
+
+
+const featureTypes = [
+	"word_id",
+	"word_type",
+	"word_position",
+	"surface_form",
+	"pos",
+	"pos_detail_1",
+	"pos_detail_2",
+	"pos_detail_3",
+	"conjugated_type",
+	"conjugated_form",
+	"basic_form",
+	"reading",
+	"pronunciation",
+	"feeling"
+];
 
 
 
@@ -19,7 +42,19 @@ window.addEventListener("DOMContentLoaded", () => {
 		}).then(res => res.json()).then(tokenized => {
 			console.log(tokenized);
 
-			//resultTable.tbodies.insertRow()
+			while (resultList.rows.length) resultList.deleteRow(0);
+
+			for (const token of tokenized) {
+				const tokenRow = resultList.insertRow();
+
+				for (let i = 0; i < featureTypes.length; i++) tokenRow.insertCell(i);
+				for (const featureName in token) tokenRow.cells[featureTypes.findIndex(name => name === featureName)].textContent = token[featureName];
+			}
 		});
+	});
+
+	clearBtn.addEventListener("click", () => {
+		textInputter.value = "";
+		M.updateTextFields();
 	});
 });
